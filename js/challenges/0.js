76bc8126201e7be24bec6610bd99a2b4ea367078
scriptmarketbiz <43850393+scriptmarketbiz@users.noreply.github.com>
2023-06-21 17:03:24 +0400

    Update 0.js

---
+++ js/challenges/0.js
@@ -0,0 +1,45 @@
+// // დაწერეთ პროგრამა წინადადებაში ყველაზე გრძელი სიტყვის სიგრძის საპოვნელად.
+
+// // Write a program to find the length of the longest word in a sentence.
+// let x="Lorem ipsum dolor sit amet, consectetur addddddipiscing elit. In pulvinar diam at tellus gravida, at.";
+// const arr=x.split(" ");
+// let ln=0;
+// let i=0;
+// console.log(arr);
+// arr.forEach((element, index) => {
+//     console.log(element+index);  
+//     if(element.length>ln)
+//     {
+//     ln=element.length; 
+//     i=index;
+//     }   
+// });
+// console.log(arr[i]);
+
+
+// დაწერეთ პროგრამა, რათა შეამოწმოთ არის თუ არა სტრიქონი პალინდრომი.
+
+// Write a program to check if a string is a palindrome.
+
+// let x=`The error you re encountering is due to a 
+// syntax issue in your JavaScript code. In JavaScript,
+//  multi-line strings cannot be directly enclosed within single 
+//  or double quotes. If you need to create a string that spans multiple lines, 
+//  you can use template literals 
+// (enclosed within backticks) or concatenate multiple strings. Here s an example:
+
+// Using template literals:
+
+//  elle
+// `;
+
+// let x1=x.replace(/[^a-zA-Z0-9]/g," ");
+// const arr=x1.split(" ");
+// let rev="";
+// arr.forEach((element, index) => {
+//     if(element===element.split("").reverse().join("") && element.trim().length>1)
+//     {
+//         rev=element.split("").reverse().join("");
+//         console.log(`${element} is palindromia ${rev}`)
+//     }
+// });
