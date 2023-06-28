22e478a91b51743e7407c39bcc03857e823fa7a0
scriptmarketbiz <43850393+scriptmarketbiz@users.noreply.github.com>
2023-06-28 12:07:26 +0400

    q

---
+++ js/challenges/1.js
@@ -0,0 +1,22 @@
+// Exercise 1: Changing Text Content
+// Create an HTML page with a <p> element. Write a JavaScript function that, when called, changes the text content of the <p> element to "Hello, DOM!".
+
+function f()
+{
+    
+}
+
+
+// Exercise 2: Changing Styles
+// Create an HTML page with a <button> element. Write a JavaScript function that, when called, changes the background color of the <button> to red and the text color to white.
+
+// Exercise 3: Creating Elements
+// Create an HTML page with an empty <div> element. Write a JavaScript function that, when called, creates a new <p> element with the text content "New paragraph" and appends it as a child to the <div>.
+
+// Exercise 4: Event Handling
+// Create an HTML page with a <button> element. Write a JavaScript function that, when the button is clicked, displays an alert with the message "Button clicked!".
+
+// Exercise 5: Form Validation
+// Create an HTML page with a <form> element containing an input field and a submit button. Write a JavaScript function that, when the form is submitted, validates the input field. If the input is empty, display an error message next to the input field. If the input is not empty, display an alert with the message "Form submitted!".
+
+// These exercises will help you practice manipulating the DOM using JavaScript. You can expand on these exercises by adding more complex interactions and functionality to further enhance your skills. Remember to link your JavaScript file to the HTML page using the <script> tag.
